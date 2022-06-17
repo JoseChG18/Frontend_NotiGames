7721397069459157704fdaeaedbc8af7b25c6cd1
itsdavyd <davi123fragoso@gmail.com>
2022-06-17 19:18:22 +0200

    Creacion NotFound 404

--- src/components/views/NotFound/NotFound.js
+++ src/components/views/NotFound/NotFound.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Header from "../Header";
 import Footer from "../Footer";
-
+import "./NotFound.scss";
 const NotFound = () => {
   return (
     <div>
       <Header />
-      NotFound
+      <div className="contPreload">
+        <div className="preloader"></div>
+        <div className="textCargando Notfound text-danger">Not Found 404</div>
+      </div>
       <Footer />
     </div>
   );
